e77e6c5414dae8f4a7479b518afb0d04ea1bdbbc
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2023-10-13 12:37:32 +0700

    final commit

--- assignment/index.js
+++ assignment/index.js
@@ -7,9 +7,24 @@ let grade4 = 74;
 let passingGrade = 80;
 //1.Create a variable called average that computes for the average from grade1 to grade4
 
+let average = (grade1 + grade2 + grade3 + grade4) / 4
+
 //2. Change the value of studentLastName to "Newman" 
+
+studentLastName = "Newman"
 
 //3. Print in console the following statement: "<value of studentFirstName> <value of studentLastName> has an average grade of <value of average>"
 //ex. Jane Doe has an average grade of 100.
 
+// let messagePart = "has an average grade of"
+
+// console.log(studentFirstName,studentLastName,messagePart,average)
+
+console.log(studentFirstName + " " + studentLastName + " " + "has an average grade of " + average)
+
 //4. Write code that would answer this question: Did the student pass?
+
+if (average >= passingGrade) {
+    console.log(studentFirstName + " " + studentLastName + " got a passing grade.")
+
+} else { console.log(studentFirstName + " " + studentLastName + " failed the test.") }
